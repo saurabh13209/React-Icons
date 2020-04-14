f6e17ae9640353d66f02921c115366888a5532bd
saurabh13209 <agrawal99saurabh@gmail.com>
2020-04-15 01:33:25 +0530

    Drawer Navigation

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/HomeScreen';
@@ -8,20 +7,36 @@ import IconInfo from './src/IconInfo';
 import TitleIcon from './src/TitleIcon';
 import Store from './src/Mobx/Store';
 import { Observer } from 'mobx-react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Contact from './src/Contact';
 
 const StackNavigator = createStackNavigator();
+const DrawerNavigator = createDrawerNavigator();
 
 export default App = () => {
+
+  const StackNavigationFunc = () => {
+    return (
+      <StackNavigator.Navigator>
+        <StackNavigator.Screen component={HomeScreen} name="Home" options={{ title: "React Icons", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+        <StackNavigator.Screen component={IconInfo} name="IconInfo" options={{ title: "Icon Info", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+        <StackNavigator.Screen component={TitleIcon} name="TitleInfo" options={{ title: Store.chatArray, headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+      </StackNavigator.Navigator>
+    );
+  }
+
   return (
     <Observer>
       {
         () => (
           <NavigationContainer>
-            <StackNavigator.Navigator>
-              <StackNavigator.Screen component={HomeScreen} name="Home" options={{ title: "React Icons", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
-              <StackNavigator.Screen component={IconInfo} name="IconInfo" options={{ title: "Icon Info", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
-              <StackNavigator.Screen component={TitleIcon} name="TitleInfo" options={{ title: Store.chatArray, headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
-            </StackNavigator.Navigator>
+            <DrawerNavigator.Navigator
+              drawerContentOptions={{ activeBackgroundColor: "#d4d4d4", inactiveBackgroundColor: 'black', labelStyle: { color: "white" }, contentContainerStyle: { backgroundColor: 'black', flex: 1 } }}
+            >
+              <DrawerNavigator.Screen component={StackNavigationFunc} name="HomeDraw" options={{ title: "Home", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+              <DrawerNavigator.Screen component={IconInfo} name="InstallDraw" options={{ title: "Installation", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+              <DrawerNavigator.Screen component={Contact} name="ContactDraw" options={{ title: "Contact Us", headerTintColor: "#fff", headerStyle: { backgroundColor: "#1F1F1F", elevation: fontCustomSize(8) } }} />
+            </DrawerNavigator.Navigator>
           </NavigationContainer>
         )
       }
